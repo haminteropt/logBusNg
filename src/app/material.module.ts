1d7315ebb57bcbae9f652b654ebc45ee0416c120
DELENN\Darryl <darryl@wagoner.me>
2018-05-17 06:49:37 -0500

    add new file

---
+++ src/app/material.module.ts
@@ -0,0 +1,61 @@
+
+import { AppComponent } from './app.component';
+import { NgModule } from '@angular/core';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import {
+    MatNativeDateModule, MatSelectModule, MatSlideToggleModule,
+    MatTabsModule, MatProgressSpinnerModule, MatRadioModule,
+    MatChipsModule, MatCardModule, MatDatepickerModule, MatInputModule,
+    MatAutocompleteModule, MatToolbarModule, MatIconModule, MatMenuModule,
+    MatCheckboxModule, MatButtonModule, MatStepperModule, MatDivider, MatDividerModule, MatCardActions
+} from '@angular/material';
+@NgModule({
+    declarations: [],
+    imports: [
+        MatButtonModule,
+        MatCheckboxModule,
+        MatMenuModule,
+        MatIconModule,
+        MatToolbarModule,
+        MatAutocompleteModule,
+        MatInputModule,
+        MatNativeDateModule,
+        MatDatepickerModule,
+        MatSelectModule,
+        MatCardModule,
+        MatChipsModule,
+        MatSlideToggleModule,
+        MatTabsModule,
+        MatProgressSpinnerModule,
+        MatRadioModule,
+        MatStepperModule,
+        MatDividerModule,
+        MatButtonToggleModule
+        
+    ],
+    exports: [
+        MatButtonModule,
+        MatCheckboxModule,
+
+        MatMenuModule,
+        MatIconModule,
+        MatToolbarModule,
+        MatAutocompleteModule,
+        MatInputModule,
+        MatNativeDateModule,
+        MatDatepickerModule,
+        MatSelectModule,
+        MatCardModule,
+        MatChipsModule,
+        MatSlideToggleModule,
+        MatTabsModule,
+        MatProgressSpinnerModule,
+        MatRadioModule,
+        MatStepperModule,
+        MatDividerModule,
+        MatButtonToggleModule
+    ]
+})
+
+export class MaterialModule { }
+
